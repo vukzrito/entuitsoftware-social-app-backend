0301027a59a8554bbf2a20650e88f2330cc923df
Rito V <vukzrito@gmail.com>
2025-05-10 00:24:15 +0200

    add paystack success endpoint

--- src/modules/payments/payments.router.ts
+++ src/modules/payments/payments.router.ts
@@ -39,4 +39,12 @@ export const paymentsRouter = Router()
     "/paystack/verify",
     authenticate,
     async (req: Request, res: Response) => {}
+  ).post(
+    "/paystack/success",
+    authenticate,
+    async (req: Request, res: Response) => {
+        console.log("req.body", req.body);
+        const { event, data } = req.body;
+        res.status(200).json({ });
+    }
   );
